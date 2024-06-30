c624d12da3e21a1def410056a5b23714b23b573a
ALprjkt <168145100+ALprjkt@users.noreply.github.com>
2024-06-30 01:01:47 +0000

    anu
    Signed-off-by: ALprjkt <168145100+ALprjkt@users.noreply.github.com>

--- upload.sh
+++ upload.sh
@@ -1,5 +1,5 @@
-if [ -f ~/rom/out/target/product/ysl/qassa*.zip ]; then
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip)"
-      rclone copy ~/rom/out/target/product/ysl/qassa*.zip ysl:qassa -P
-      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls qassa*.zip) Uploaded Successfully!"
+if [ -f ~/rom/out/target/product/vince/lineage*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/vince/ && ls lineage*.zip)"
+      rclone copy ~/rom/out/target/product/ysl/qassa*.zip ysl:ROM -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/vince/ && ls lineage*.zip) Uploaded Successfully!"
 fi
