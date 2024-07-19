b7687d94e55e9cd8c545ad876d8417d04b8893cf
animelopers <164208514+animelopers@users.noreply.github.com>
2024-07-19 16:35:04 +0700

    ci: xdCLO ysl
    Signed-off-by: animelopers <164208514+animelopers@users.noreply.github.com>

---
+++ upload.sh
@@ -0,0 +1,15 @@
+if [ -f ~/rom/out/target/product/ysl/xdCLO*.zip ]; then
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Uploading Build $(cd ~/rom/out/target/product/ysl/ && ls xdCLO*.zip)"
+      rclone copy ~/rom/out/target/product/ysl/CrDroid*.zip rom:xd -P
+      curl -s https://api.telegram.org/$tokentl/sendMessage -d chat_id=$idtl -d text="Build $(cd ~/rom/out/target/product/ysl/ && ls xdCLO*.zip) Uploaded Successfully!"
+fi
+
+if [ -f ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb ]; then
+      rclone copy ~/rom/out/target/product/ysl/obj/KERNEL_OBJ/arch/arm64/boot/Image.gz-dtb rom:xd -P
+fi
+
+if [ -f ~/rom/out/error.log ]; then
+      LOG=~/rom/out/error.log
+      curl -F document=@$LOG https://api.telegram.org/$tokentl/sendDocument?chat_id=$idtl \
+      -F caption="Check these error logs"
+fi
