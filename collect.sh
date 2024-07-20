b2d7ba5208fe3129dac69dfa3f7abf4f57a26b60
animelopers <164208514+animelopers@users.noreply.github.com>
2024-07-20 13:22:45 +0700

    Update collect.sh [skip-ci]

--- collect.sh
+++ collect.sh
@@ -6,9 +6,9 @@ export TZ=Asia/Jakarta
 export CCACHE_DIR=~/ccache
 export CCACHE_EXEC=$(which ccache)
 export USE_CCACHE=1
-export BUILD_USERNAME="rahmad"
-export BUILD_HOSTNAME="IDN-Labs"
-ccache -M 100G -F 0
+export BUILD_USERNAME="alprjt"
+export BUILD_HOSTNAME="cirrus-ci"
+ccache -M 50G -F 0
 ccache -o compression=true
 ccache -z
 $lunch
